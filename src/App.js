cd3cfbce07944f205961520c919553932a4d3016
Rojotiana Nomena <hei.nomena@gmail.com>
2022-10-14 13:40:57 +0300

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { Header } from "./components/Header";
-import './styles/header.css' ;
-import { Footer } from "./components/Footer";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import BookList from './pages/BookList';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <>      
+      <Header/>
       <div className="App">
-        <Header/>
-        <Footer/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booklist" element={<BookList />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
+      <Footer/>
     </>
   );
 }
