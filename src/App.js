839f85ba91e76ea7336986db40beba28cf94fd36
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-10-14 11:32:32 +0300

    feat: adding footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Header } from "./components/Header";
 import './styles/header.css' ;
+import { Footer } from "./components/Footer";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<App/>}/>
-      </Routes> 
-      
+    <>      
       <div className="App">
         <Header/>
+        <Footer/>
       </div>
     </>
   );
